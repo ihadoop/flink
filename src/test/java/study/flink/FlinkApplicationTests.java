58843adccb9b8c8e883e8fe2f030f4bf4e891059
13574798@qq.com <13574798@qq.com>
2024-05-15 19:56:40 +0800

    init

---
+++ src/test/java/study/flink/FlinkApplicationTests.java
@@ -0,0 +1,13 @@
+package study.flink;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlinkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
